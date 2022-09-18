e0dc2c01b9ffad85678d03fa00c39818912fc4bc
ostov-larion <39136591+ostov-larion@users.noreply.github.com>
2022-09-18 23:12:53 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${port}`))
+  .listen(port, () => console.log(`Listening on ${port}`))
 
 let broker = new ws.Server({port})
 
