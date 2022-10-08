91c1b25156d0b75ae27269a60253a18f52d468ba
ostov-larion <39136591+ostov-larion@users.noreply.github.com>
2022-10-08 08:13:00 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -9,29 +9,21 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(port, () => console.log(`Listening on ${port}`))
 
-let broker = new ws.Server({server})
+let broker = new ws.Server({
+  server
+})
 
 console.log(`Broker started on port: ${port}.`)
-
-let subs = []
 
 let cons = []
 
 broker.on('connection', client => {
     cons.push(client)
     client.on('message', m => {
-        let [_h, body] = m.toString().split('|')
-        let [type, $topic] = _h.split(' ')
-        console.log(type, $topic)
-        if(type == 'pub')
-            subs.filter(([con, topic]) => topic == $topic && con != client).forEach(([con]) => con.send(body))
-        if(type == 'sub') {
-            subs.push([client, $topic])
-            cons.filter(con => con != client).forEach(con => con.send(`greet ${$topic}`))
-        }
+        if(m == 'ping') return null
+        cons.filter(con => con != client).forEach(con => con.send(m.toString()))
     })
     client.on('close', () => {
-        subs = subs.filter(([con, _]) => con != client)
         cons = cons.filter(con => con != client)
     })
 })
